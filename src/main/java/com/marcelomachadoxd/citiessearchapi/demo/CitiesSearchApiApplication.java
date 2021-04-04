2e979a47e1b623b82f9cd8b9ea3665de3a5de81f
MarceloMachadoxD <mastermanson@gmail.com>
2021-04-04 17:53:22 -0300

    first commit

---
+++ src/main/java/com/marcelomachadoxd/citiessearchapi/demo/CitiesSearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.marcelomachadoxd.citiessearchapi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesSearchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitiesSearchApiApplication.class, args);
+    }
+
+}
