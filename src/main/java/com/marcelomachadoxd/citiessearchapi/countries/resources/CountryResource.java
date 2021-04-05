8dd85d92bf3d1b2f8c12d2dd5949880da96ff683
MarceloMachadoxD <mastermanson@gmail.com>
2021-04-05 20:05:16 -0300

    countries Pageable in /countries/page

--- src/main/java/com/marcelomachadoxd/citiessearchapi/countries/resources/CountryResource.java
+++ src/main/java/com/marcelomachadoxd/citiessearchapi/countries/resources/CountryResource.java
@@ -1,9 +1,11 @@
 package com.marcelomachadoxd.citiessearchapi.countries.resources;
 
-import antlr.ASTNULLType;
 import com.marcelomachadoxd.citiessearchapi.countries.entities.Country;
 import com.marcelomachadoxd.citiessearchapi.countries.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +22,11 @@ public class CountryResource {
     @GetMapping
     public List<Country> countries(){
         return repository.findAll();
+    }
+
+    @GetMapping("/page")
+    public Page<Country> countries(Pageable page){
+        return repository.findAll(page);
     }
 
 }
