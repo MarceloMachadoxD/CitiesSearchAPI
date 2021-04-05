65215974fc69eb9a2996562862dc01de50cec9f5
MarceloMachadoxD <mastermanson@gmail.com>
2021-04-05 20:32:32 -0300

    Adjust http response to 404 not found instead 500 server error when a country not found by id

--- src/main/java/com/marcelomachadoxd/citiessearchapi/countries/resources/CountryResource.java
+++ src/main/java/com/marcelomachadoxd/citiessearchapi/countries/resources/CountryResource.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -27,6 +30,17 @@ public class CountryResource {
     @GetMapping("/page") //request-example http://localhost:8080/countries/page?page=0&size=10&sort=name,asc
     public Page<Country> countries(Pageable page){
         return repository.findAll(page);
+    }
+    @GetMapping("/{id}") // request-example http://localhost:8080/countries/1
+    public ResponseEntity getOne(@PathVariable Long id){
+        Optional<Country> optional = repository.findById(id);
+
+        if (optional.isPresent()){
+            return ResponseEntity.ok().body(optional.get());
+
+        } else{
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
