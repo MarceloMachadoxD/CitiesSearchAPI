9462f38d0e1767fcb17b271fa800391c3aa4df81
MarceloMachadoxD <mastermanson@gmail.com>
2021-04-05 20:05:48 -0300

    countries Pageable in /countries/page

--- src/main/java/com/marcelomachadoxd/citiessearchapi/countries/repository/CountryRepository.java
+++ src/main/java/com/marcelomachadoxd/citiessearchapi/countries/repository/CountryRepository.java
@@ -1,11 +1,7 @@
 package com.marcelomachadoxd.citiessearchapi.countries.repository;
-
 import com.marcelomachadoxd.citiessearchapi.countries.entities.Country;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Pageable;
 
 public interface CountryRepository extends JpaRepository<Country, Long> {
 
