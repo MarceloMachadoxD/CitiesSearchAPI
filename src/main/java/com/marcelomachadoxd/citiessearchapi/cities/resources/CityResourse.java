93e059c0ced1952e41b364ee7f7fddbfff228d1b
MarceloMachadoxD <mastermanson@gmail.com>
2021-04-07 22:40:41 -0300

    Remove unused imports

--- src/main/java/com/marcelomachadoxd/citiessearchapi/cities/resources/CityResourse.java
+++ src/main/java/com/marcelomachadoxd/citiessearchapi/cities/resources/CityResourse.java
@@ -3,14 +3,12 @@ package com.marcelomachadoxd.citiessearchapi.cities.resources;
 import com.marcelomachadoxd.citiessearchapi.cities.entities.City;
 import com.marcelomachadoxd.citiessearchapi.cities.repositories.Cityrepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("cities")
